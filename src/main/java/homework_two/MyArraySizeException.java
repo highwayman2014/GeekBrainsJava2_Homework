1af286a525eb0bf2e79c04efebe654d7abc4748c
highwayman2014 <shepardalex2012@gmail.com>
2021-03-19 09:36:25 +0300

    Revert "finish hw 2"
    This reverts commit 4f3be9426e9f7223cd3ba68918f8f40ce4ab6887.

--- src/main/java/homework_two/MyArraySizeException.java
+++
@@ -1,9 +0,0 @@
-package homework_two;
-
-public class MyArraySizeException extends Exception{
-
-    public MyArraySizeException(String message) {
-        super(message);
-        System.err.println(message);
-    }
-}
